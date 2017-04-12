cfb7f9fad4e3cae4d1360d85666f15fdce436467
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2017-04-12 14:34:05 +0200

    TourOfHeroes lesson 4 done ng training 1 excercise

---
+++ JS/ng-training/src/app/books/book-details/book-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Book} from '../book';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent implements OnInit {
+
+  currentBook: Book;
+
+  constructor() {
+    this.currentBook = new Book();
+  }
+
+  ngOnInit() {
+    this.currentBook.authors = 'Zuzanna';
+    this.currentBook.title = 'Mlodosc';
+  }
+
+}
